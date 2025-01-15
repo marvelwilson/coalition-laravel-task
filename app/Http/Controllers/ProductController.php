d2cb2921c357049cc9b89abcbe3fe939a6683c49
marvelwilson <marvelwilsononit@gmail.com>
2025-01-15 01:22:08 +0100

    concluded

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,52 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private $jsonFile = 'products.json';
+
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function addProduct(Request $request)
+    {
+        $data = $request->all();
+        $data['datetime'] = now()->toDateTimeString();
+        $data['total_value'] = $data['quantity'] * $data['price'];
+        
+        // Read existing data
+        $products = json_decode(file_get_contents(storage_path($this->jsonFile)), true) ?? [];
+        $products[] = $data;
+        
+        // Save new data
+        file_put_contents(storage_path($this->jsonFile), json_encode($products, JSON_PRETTY_PRINT));
+
+        return response()->json(['success' => true]);
+    }
+
+    public function getProducts()
+    {
+        $products = json_decode(file_get_contents(storage_path($this->jsonFile)), true) ?? [];
+        return response()->json($products);
+    }
+
+    public function editProduct(Request $request)
+{
+    $products = json_decode(file_get_contents(storage_path($this->jsonFile)), true);
+    $index = $request->input('index'); // Get index from request
+    
+    if (isset($products[$index])) {
+        // Update product details
+        $products[$index]['productName'] = $request->input('productName');
+        $products[$index]['quantity'] = $request->input('quantity');
+        $products[$index]['price'] = $request->input('price');
+        $products[$index]['datetime'] = now()->toDateTimeString(); // Update timestamp
+        $products[$index]['total_value'] = $products[$index]['quantity'] * $products[$index]['price'];
+
+        // Save updated data
+        file_put_contents(storage_path($this->jsonFile), json_encode($products, JSON_PRETTY_PRINT));
+    }
+
+    return response()->json(['success' => true]);
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index']);
+Route::post('/addProduct', [ProductController::class, 'addProduct']);
+Route::get('/getProducts', [ProductController::class, 'getProducts']);
+Route::post('/editProduct', [ProductController::class, 'editProduct']);
+
